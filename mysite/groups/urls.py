357b946f068215d77a81ef5a08c1dcd34aa630b8
MaarkNassef <maark.nassef@gmail.com>
2024-01-23 17:49:33 +0200

    Delete group.

--- mysite/groups/urls.py
+++ mysite/groups/urls.py
@@ -6,4 +6,5 @@ app_name = 'groups'
 urlpatterns = [
     path('add/', views.GroupCreateView.as_view(), name="add_group"),
     path('<int:pk>/', views.GroupDetailView.as_view(), name="group"),
+    path('<int:pk>/delete/', views.GroupDeleteView.as_view(), name="delete_group"),
 ]
--- mysite/groups/views.py
+++ mysite/groups/views.py
@@ -1,6 +1,5 @@
-from typing import Any
 from django.shortcuts import redirect
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Group
@@ -24,3 +23,14 @@ class GroupCreateView(LoginRequiredMixin, CreateView):
 class GroupDetailView(LoginRequiredMixin, DetailView):
     model = Group
     template_name = 'groups/detail.html'
+
+class GroupDeleteView(LoginRequiredMixin, DeleteView):
+    model = Group
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        group = self.get_object()
+        if group.admin == request.user:
+            group.delete()
+            
+        return redirect(self.success_url)

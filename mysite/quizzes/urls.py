28af4d7e3f145f10544252f7c6f084ed76edabfb
MaarkNassef <maark.nassef@gmail.com>
2024-03-05 18:46:58 +0200

    Showing questions for teachers.

--- mysite/quizzes/urls.py
+++ mysite/quizzes/urls.py
@@ -5,4 +5,7 @@ app_name = 'quizzes'
 
 urlpatterns = [
     path('<int:pk>/', views.QuizDetailView.as_view(), name='QuizDetailView'),
+    path('create/<int:group_id>/', views.QuizCreateView.as_view(), name='QuizCreateView'),
+    path('questions/<int:quiz_id>/', views.ShowQuestions, name='ShowQuestions'),
+    path('questions/delete/<int:pk>/', views.QuestionDeleteView.as_view(), name='QuestionDeleteView'),
 ]
--- mysite/quizzes/views.py
+++ mysite/quizzes/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView
 from .models import Quiz, Question, Choice, Grade
+from groups.models import Group
+from datetime import datetime, timedelta
+from django.http import JsonResponse
+import json
+
 
 class QuizDetailView(DetailView):
     model = Quiz
@@ -16,7 +22,8 @@ class QuizDetailView(DetailView):
         for question in quiz.question_set.all():
             right_answer_id = question.choice_set.filter(is_correct=True).values_list('id', flat=True).first()
             selected_answer = request.POST.get(str(question.id))
-            selected_answer_id = -1 if selected_answer is None else int(selected_answer[0])
+
+            selected_answer_id = -1 if selected_answer is None else int(selected_answer)
             if right_answer_id == selected_answer_id:
                 right_answers +=1
 
@@ -25,3 +32,61 @@ class QuizDetailView(DetailView):
 
         group_id = quiz.group.id
         return HttpResponseRedirect(reverse_lazy('groups:group', kwargs={'pk':group_id }))
+    
+class QuizCreateView(CreateView):
+    model = Quiz
+    template_name = 'quizzes/create.html'
+    fields = ["title", "start_time", "duration"]
+
+    def dispatch(self, request, *args, **kwargs):
+        self.group_id = kwargs['group_id']
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["group_id"] = self.group_id
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get('title')
+        date = request.POST.get('date')
+        start_time = request.POST.get('start_time')
+        duration = request.POST.get('duration')
+        date = date.split('-')
+        start_time = start_time.split(':')
+        group_id = self.group_id
+        group = Group.objects.get(pk=int(group_id))
+        quiz = Quiz(title=title, 
+                    start_time=datetime(year=int(date[0]), 
+                                        month=int(date[1]), 
+                                        day=int(date[2]), 
+                                        hour=int(start_time[0]), 
+                                        minute=int(start_time[1])), 
+                    duration=timedelta(minutes=int(duration)),
+                    group=group)
+        quiz.save()
+        return HttpResponseRedirect(reverse_lazy('groups:group', kwargs={'pk':group_id }))
+    
+def ShowQuestions(request, quiz_id):
+    quiz = Quiz.objects.get(pk=quiz_id)
+    if request.method == 'GET':
+        return render(request, 'quizzes/questions.html', {'quiz':quiz})
+    
+    data = json.loads(request.body.decode('utf-8'))
+    question_text = data.get('question')
+    choices = data.get('choices')
+    question = Question(text=question_text, quiz=quiz)
+    question.save()
+    for choice in choices:
+        c = Choice(text=choice['value'], is_correct=choice['checked'], question=question)
+        c.save()
+    return JsonResponse({'success': True})
+
+class QuestionDeleteView(DeleteView):
+    model = Question
+    
+    def get(self, request: HttpRequest, *args, **kwargs):
+        question = self.get_object()
+        quiz = question.quiz
+        question.delete()
+        return HttpResponseRedirect(reverse_lazy('quizzes:ShowQuestions', kwargs={'quiz_id':quiz.id}))
